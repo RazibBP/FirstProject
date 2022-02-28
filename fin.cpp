ff746f450df36399b37b08fa3e15b8eb1fa4c54f
RazibBP <mdrazib.dev@gmail.com>
2022-02-28 19:24:20 +0600

    add

---
+++ fin.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main ()
+{
+	double cels;
+	cout << "Enter Your Far.no :";
+	cin >> cels;
+	cout << endl;
+
+	double far=1.8 * cels + 32;
+       cout << "Your Result : "	<< far;
+       cout << endl;
+}
