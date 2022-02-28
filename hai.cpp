de5a2c7ec8fa26c5ef9c62fd98a3ac317d1a6410
RazibBP <mdrazib.dev@gmail.com>
2022-02-28 19:10:05 +0600

    add

---
+++ hai.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+
+{
+	double base,hight;
+	
+
+	cout << "Enter Your Base : ";
+	cin >> base;
+	cout << endl;
+
+
+	cout << "Enter Your Hight : ";
+	cin >> hight;
+	cout << endl;
+
+	double Area= 0.5 * base * hight;
+	cout << "Your Result : " << Area;
+	cout << endl;
+}
