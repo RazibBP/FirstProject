6d1c4028cc2a8653f2c50c16e9fb666afbb0359d
RazibBP <mdrazib.dev@gmail.com>
2022-02-28 18:57:30 +0600

    add

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,40 @@
 #include<iostream>
+#include<iomanip>
 
 using namespace std;
 
 int main()
 {
-	cout << "Hello World."<<  endl;
+	int num1,num2;
+
+	cout << showpoint;
+	cout << fixed;
+	cout << setprecision(2);
+	
+	cout<< "Please Enter Two Namber :";
+	cin >> num1 >> num2;
+
+	int sum=num1+num2;
+	cout <<setw(15) << "Your Result= "<<sum;
+	cout << endl;
+
+	int sub=num1-num2;
+	cout << "Your Result= " << sub;
+	cout << endl;
+	
+	cout << noshowpoint;
+
+	double div= (float) num1 / num2;
+	cout << "Divistion no :" <<div;
+	cout << endl;
+
+	int  vib= num1 * num2;
+	cout << "Miltiplision no: " <<vib;
+	cout << endl;
+
+	int rim=num1 % num2;
+	cout << "Remainder no : "<< rim;
+	cout << endl;
+
 	return 0;
 }
