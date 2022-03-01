382a936a376a545ef6c4e1de998b7148ed573747
RazibBP <mdrazib.dev@gmail.com>
2022-03-01 22:32:06 +0600

    This Program is Vowel And Consonant

---
+++ vowel-consonant.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	char ch;
+
+	cout << "Enter Type A Latter :";
+	cin >> ch;
+	cout << endl;
+
+	switch (ch)
+	{
+		case 'a':
+			cout << "Vowel";
+		break;
+
+		case 'e':
+			cout << "Vowel";
+		break;
+
+		case 'i':
+			cout << "Vowel";
+		break;
+
+		case 'o':
+			cout << "Vowel";
+		break;
+
+		case 'u':
+			cout << "Vowel";
+		break;
+
+		case 'A':
+			cout << "Vowel";
+		break;
+
+		case 'E':
+			cout << "Vowel";
+		break;
+
+		case 'I':
+			cout << "Vowel";
+		break;
+
+		case 'O':
+			cout << "Vowel";
+		break;
+
+		case'U':
+			cout << "Vowel";
+		break;
+
+		default:
+			cout << "Consonant";
+
+		return 0;
+
+		
+	
+	}
+}
