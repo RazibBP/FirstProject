ccbf15fe944adabdb094bd8a68feb79958eaaa6b
RazibBP <mdrazib.dev@gmail.com>
2022-03-02 20:45:09 +0600

    This file is For While Do-While Loop

---
+++ for-while-do-while.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int  num,i;
+	cout << "Enter a type a number :";
+	cin >> num;
+	cout << endl;
+
+	for (i=1; i<=num; i++)
+	{
+		cout << "Bngladesh is a small country";
+		cout << endl;
+	}
+	
+	i=1;
+	while (i<=num)
+	{
+		cout << "I Love my country:";
+		cout << endl;
+		i++;
+	}
+
+	i=1;
+	do {
+		cout << "I Love this person";
+		cout << endl;
+		i++;
+	}while (i<=num);
+
+	return 0;
+
+
+
+}
